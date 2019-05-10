f6a7f80aff7585de74862661764305d5137dfee2
Anthony <anthonymglick@gmail.com>
2019-05-09 20:07:28 -0400

    scaffolded components and views, created auth.js and set up router

--- front-end/src/router.js
+++ front-end/src/router.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import auth from './shared/auth'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
@@ -14,12 +15,28 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/login',
+      name: 'login',
+      component: () => import('./views/Login.vue'),
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  // Determine if the route requires Authentication
+  const requiresAuth = to.matched.some(x => x.meta.requiresAuth);
+  const user = auth.getUser();
+
+  // If it does and they are not logged in, send the user to "/login"
+  if (requiresAuth && !user) {
+    next("/login");
+  } else {
+    // Else let them go to their next destination
+    next();
+  }
+});
+
+export default router;
